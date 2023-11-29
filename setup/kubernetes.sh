eaa5da8cad691c3848a43bdfc200746d5dd8facb
Soufiane Chahid <schahid@Soufianes-MacBook-Air.local>
2023-11-29 15:07:57 +0100

    fix script and manifest

--- setup/kubernetes.sh
+++ setup/kubernetes.sh
@@ -1,16 +1,15 @@
+#! /bin/bash
+
+# Install Kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
 # Install k3d
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
 #expose port 8080 for ArgoCD UI and 9999 for Netflix-app
 k3d cluster create -p 8080:80@loadbalancer -p 9999:30007@loadbalancer
-
-# Install Kubectl
-sudo apt -y update
-sudo apt -y install -y apt-transport-https ca-certificates curl
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt -y update
-sudo apt -y install kubectl
 
 # Create namespaces
 kubectl create namespace argocd
